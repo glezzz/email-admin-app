b0ef3291cb06a8b3658665d3e2392f9f07316932
glezzz <alexglezk@gmail.com>
2021-01-13 16:28:50 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+###Email admin app
+<p>This is small project to continue practicing Java. The application does the following:</p> 
+
+- Generates an email with the following syntax: *firstname.lastname@department.company.com*
+- Determine the department (sales, development, accounting), if none, leave blank 
+- Have set methods to change the password, set the mailbox capacity, and define and alternate email address 
+- Have get methods to display the name, email and mailbox capacity
---
+++ src/emailapp/Email.java
@@ -0,0 +1,68 @@
+package emailapp;
+
+import java.util.Scanner;
+
+public class Email {
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private int mailboxCapacity;
+    private String alternateEmail;
+
+    // Constructor to receive the first name and last name
+    public Email(String firstName, String lastName) {
+        // 'this.firstName' refers to class level variable, 'firstName' refers to local variable
+        this.firstName = firstName;
+        this.lastName = lastName;
+        System.out.println("EMAIL CREATED: " + this.firstName + " " + this.lastName);
+
+        // call a method asking for the department and return it
+        this.department = setDepartment();
+        System.out.println("Department: " + this.department);
+
+        // call a method to generate random password
+    }
+        //ask for department
+    private String setDepartment() {
+        System.out.print("DEPARTMENT CODES:\n 1 for Sales\n 2 for Development\n 3 for Accounting\n 0 for None\n Enter your department code:");
+        //this is how we take input from the console
+        Scanner in = new Scanner(System.in);
+        int depChoice = in.nextInt();
+
+        if (depChoice == 1) {
+            return "Sales";
+
+        } else if (depChoice == 2) {
+            return "Dev";
+
+        } else if (depChoice == 3) {
+            return "Accounting";
+
+        } else {
+            return "";
+        }
+    }
+        // generate a random password
+    private String randomPassword(int length) {
+        //characters password can be made out of
+        String passwordSet = "abcdefghijklmnopqrstuvwxyz0123456789_&@!?#$%";
+                //a new sequence of characters of the size "length"
+        char[] password = new char[length];
+        for ( int i = 0; i < length; i++) {
+            //generate random number between 0 and 1 multiplied by however many characters in 'passwordSet', make that
+            //an integer and store in randNum
+            int randNum = (int) (Math.random() * passwordSet.length());
+            //take the character at randNum in passwordSet and pass it to password array one at a time
+            password[i] = passwordSet.charAt(randNum);
+        }
+        //because password is a set of characters we modify to new String
+        return new String(password);
+    }
+
+    //set the mailbox capacity
+
+    //set the alternate email
+
+    //change the password
+}
